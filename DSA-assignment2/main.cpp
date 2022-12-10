629a6e2012157173d068ea098125a452e8f32b6d
LanceloT7730 <ac2952602@akfauniversity.org>
2022-12-10 10:20:39 +0500

    Initial Commit

---
+++ DSA-assignment2/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  DSA-assignment2
+//
+//  Created by DONIYORBEK IBROKHIMOV on 10/12/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
