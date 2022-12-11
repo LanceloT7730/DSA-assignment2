dff9f9b81e866932076d7503b9165ccf07050e88
LanceloT7730 <ac2952602@akfauniversity.org>
2022-12-11 20:52:05 +0500

    Bubble and Selection sort completed

--- DSA-assignment2/main.cpp
+++ DSA-assignment2/main.cpp
@@ -10,88 +10,146 @@
 
 using namespace std;
 
-template <class Type>
+
 struct Node {
-    Type data;
+    int data;
     Node *next;
 
     Node() {
         data = 0;
         next = NULL;
+
     }
 };
 
-template <class Type>
-class LinkedListBubbleSort {
+
+class LinkedList {
 private:
-    Node<Type> *head;
+    Node *head, *last;
 public:
 
-    LinkedListBubbleSort() {
+    LinkedList() {
         head = NULL;
+        last = NULL;
     }
 
-    Type BubleSort(vector<Type>, unsigned int size) {
-        Node<Type> *curr;
-        Node<Type> *prev;
-        if(head && head->next) {
-            curr = head->next;
-            prev = head;
-        } else if (head) {
-            curr = head;
-            prev = head;
-        } else {
-            cout << "Empty list\n";
-            return;
+    int size() {
+        Node *temp = head;
+        int counter = 0;
+
+        while (temp) {
+            counter++;
+            temp = temp->next;
         }
+        return counter;
+    }
 
-        bool flag = false;
-        Type counter = 0;
 
-        while (prev) {
-            while(curr) {
-                if(++counter && (prev->data < curr->data) ) {
+    void BubbleSort() {
+        Node *curr = head;
+        Node *nextNode = head->next;
+        int counter = size();
+        while (counter) {
+            if (curr->data > nextNode->data) swapNodes(curr, nextNode);
+            curr = curr->next;
+            nextNode = nextNode->next;
 
-                }
+            if (!nextNode) {
+                counter--;
+                curr = head;
+                nextNode = head->next;
             }
         }
     }
 
-    void swapLists(Node<Type> *prev, Node<Type> *curr) {
-        Node<Type> *temp = curr->next;
-        curr->next = prev;
-        prev->next = temp;
-
+    void swapNodes(Node *curr, Node *next) {
+        int temp = curr->data;
+        curr->data = next->data;
+        next->data = temp;
     }
 
-    void insertAtFront(int data) {
-        auto *newNode = new Node<Type>;
+    void insertAtLast(int data) {
+        auto *newNode = new Node;
         newNode->data = data;
-        newNode->next = head;
-        head = newNode;
-    }
 
-    void printAll() {
-        Node<Type> *temp = head;
-
-        while (temp) {
-            cout << temp->data << endl;
-            temp = temp->next;
+        if (!head) {
+            head = newNode;
+            last = newNode;
+        } else {
+            last->next = newNode;
+            last = newNode;
         }
     }
 
+    void printAll() {
+        Node *temp = head;
+
+        while (temp) {
+            cout << temp->data << " ";
+            temp = temp->next;
+        }
+        cout << endl;
+    }
+
+    int getMin(Node *curr) {
+        Node *temp = curr;
+        int min;
+        if (!head) {
+            cout << "Empty list\n";
+            return 0;
+        } else if(!curr) {
+            cout << "Reached the end of the list\n";
+            return 0;
+        }
+        min = curr->data;
+
+        while (temp) {
+            if (temp->data <= min) {
+                min = temp->data;
+            }
+            temp = temp->next;
+        }
+        return min;
+    }
+
+    void SelectionSort() {
+        if (!head) {
+            cout << "Empty list";
+            return;
+        }
+
+        Node *tempHead = head;
+        Node *iterator = head;
+        int min = 0;
+        while (tempHead) {
+            min = getMin(tempHead);
+            while (iterator->data != min) {
+                iterator = iterator->next;
+            }
+            swapNodes(tempHead, iterator);
+            tempHead = tempHead->next;
+            iterator = tempHead;
+        }
+
+    }
 };
 
 
 int main() {
 
-    LinkedListBubbleSort<int> list1;
-    list1.insertAtFront(4);
+    LinkedList list1;
 
-    for (int i = 0; i < 10; i++) {
-        list1.insertAtFront(i);
-    }
+    list1.insertAtLast(5);
+    list1.insertAtLast(4);
+    list1.insertAtLast(3);
+    list1.insertAtLast(2);
+    list1.insertAtLast(1);
+    list1.insertAtLast(1);
+    list1.printAll();
 
+    list1.SelectionSort();
+    cout << "After sorting\n";
+    list1.printAll();
 
 
     return 0;
